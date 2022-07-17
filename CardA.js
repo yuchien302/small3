b0ffb7787953f73811959ddb8f0fb6457ff6aa94
Yu-Chien Chan <yuchien302@gmail.com>
2022-07-17 02:02:03 -0700

    P1 (#165)

--- CardA.js
+++ CardA.js
@@ -1,7 +1,7 @@
 import AwesomeComponent from "AwesomeComponent";
 import { enable_awesome_component } from "feature_flag";
 
-// CardA is ing AAA 123
+// CardA is ing AAA 123123
 export default function CardA() {
   if (enable_awesome_component) {
     return <AwesomeComponent />;
