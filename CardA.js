e8630a4142e376230968aaffd1bd0f8a9213e953
Yu-Chien Chan <yuchien302@gmail.com>
2022-07-21 16:44:23 -0700

    Cleaup

--- CardA.js
+++ CardA.js
@@ -1,7 +1,6 @@
 import AwesomeComponent from "AwesomeComponent";
 import { enable_awesome_component } from "feature_flag";
 
-// CardA is in2122g AAA 123123
 export default function CardA() {
   if (enable_awesome_component) {
     return <AwesomeComponent />;
--- CardC.js
+++ CardC.js
@@ -1,7 +1,6 @@
 import AwesomeComponent from "AwesomeComponent";
 import { enable_awesome_component } from "feature_flag";
 
-// CardC is doing CCC
 export default function CardC() {
   if (enable_awesome_component) {
     return <AwesomeComponent />;
