99b50181be60fcbc5de76189ec8f513dbc9a4c30
Yu-Chien Chan <yuchien302@gmail.com>
2022-07-26 23:24:00 -0700

    P2

---
+++ CardC copy.js
@@ -0,0 +1,9 @@
+import AwesomeComponent from "AwesomeComponent";
+import { enable_awesome_component } from "feature_flag";
+
+export default function CardC() {
+  if (enable_awesome_component) {
+    return <AwesomeComponent />;
+  }
+  return null;
+}
