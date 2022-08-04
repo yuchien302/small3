b07654e4d605d9cd56f8ed1cd59486c2c17160e1
Yu-Chien Chan <yuchien302@gmail.com>
2022-08-03 21:09:30 -0700

    temp2

--- CardA copy.js
+++ CardA copy.js
@@ -2,3 +2,7 @@ import AwesomeComponent from "AwesomeComponent";
 import { enable_awesome_component } from "feature_flag";
 
 export default function CardA() {
+  if (enable_awesome_component) {
+    return <AwesomeComponent />;
+  }
+  return null;
