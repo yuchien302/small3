faafa812488beb0102f57d147a1ae83d91de543a
Yu-Chien Chan <yuchien302@gmail.com>
2022-08-16 23:16:11 -0700

    temp1

--- CardC copy.js
+++ CardC copy.js
@@ -4,7 +4,3 @@ import { enable_awesome_component } from "feature_flag";
 export default function CardC() {
   if (enable_awesome_component) {
     return <AwesomeComponent />;
-  }
-  // 12as3
-  return null;
-}
